c386467b48322c97a9970aeaf3a2e042dfac8329
Filipe Kiss <hello@filipekiss.com.br>
2022-06-20 10:53:33 +0200

    feat(completion): Enable op completion

--- config/compdefs.zsh
+++ config/compdefs.zsh
@@ -12,3 +12,4 @@ compdef '_files' transfer
 (( $+functions[_yarn] )) && compdef y=yarn
 (( $+functions[_youtube-dl] )) && compdef yt2mp3=youtube-dl
 (( $+commands[mx] )) && compdef '_mx' mx
+(( $+commands[op] )) && eval "$(op completion zsh)" && compdef _op op
